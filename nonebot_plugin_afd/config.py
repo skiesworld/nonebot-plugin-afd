49076cde999b8b304a4a22e4afba4a93164f7f58
17TheWord <17theword@gmail.com>
2023-12-14 12:51:52 +0800

    更新配置文件

--- nonebot_plugin_afd/config.py
+++ nonebot_plugin_afd/config.py
@@ -1,7 +1,7 @@
 from typing import Optional, Dict
 
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, Field
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    afd_token_list: Optional[Dict[str, Dict[str, str]]] = None
+    afd_token_list: Optional[Dict[str, Dict[str, str]]] = Field(default_factory=dict)
