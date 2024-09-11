896cd5781ccb89cb83ac1eb2517a6aa8e935e091
17TheWord <17theword@gmail.com>
2024-09-11 21:13:47 +0800

    优化群聊新成员自动同意逻辑
    优化了群聊新成员自动同意逻辑，通过移除多余循环，简化条件检查，从而提高处理效率。更新了日志信息格式，改善代码的可读性和维护性。

--- nonebot_plugin_afd/group_new_member.py
+++ nonebot_plugin_afd/group_new_member.py
@@ -74,21 +74,14 @@ async def _(bot: Bot, event: GroupRequestEvent):
                 logger.debug(f"已将用户 {event.user_id} 通知发送至群聊 {event.group_id}")
                 raise FinishedException
 
-            for order in order_response.data.list:
-                if order.out_trade_no == comment:
-                    delay = random.uniform(3, 5)
-                    logger.debug(
-                        f"用户 {event.user_id} 的订单号 {comment} 数据列表不为空，将在 {delay:.2f} 秒后同意请求"
-                    )
-                    await asyncio.sleep(delay)
-                    await event.approve(bot)
-                    logger.debug(f"用户 {event.user_id}，使用订单号 {comment}，加入群聊 {event.group_id}")
-                    raise FinishedException
-            else:
-                logger.info(
-                    f"用户 {event.user_id} 的订单号 {comment} 不属于群聊 {event.group_id} 的作者 {afdian_bot.bot_info.user_id}，"
-                    f"将遍历下一位作者"
-                )
+            delay = random.uniform(3, 5)
+            logger.debug(
+                f"用户 {event.user_id} 的订单号 {comment} 数据列表不为空，将在 {delay:.2f} 秒后同意请求"
+            )
+            await asyncio.sleep(delay)
+            await event.approve(bot)
+            logger.debug(f"用户 {event.user_id}，使用订单号 {comment}，加入群聊 {event.group_id}")
+            raise FinishedException
 
     else:
         msg = f"用户 {event.user_id} 的订单号不属于群聊 {event.group_id} 的任何作者，将忽略"
